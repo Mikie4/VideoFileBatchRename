90cc2a390c9c19ccff976de2dbeae6f477084131
Michael <Mikie4@gmail.com>
2019-02-28 14:46:16 -0500

    Added -Title -Season -FixTitle -FixSeason

--- VideoFileBatchRename.ps1
+++ VideoFileBatchRename.ps1
@@ -1,1 +1,92 @@
-﻿
+﻿param([String]$title='UNNAMED',[String]$season='01',[String]$FixSeason="",[String]$FixTitle="")
+
+# Create Test Files #
+#ForEach($x in 95..125){$y = $x-100;New-Item -ItemType 'file' -Path .\TestFiles -Name "$x Title $y";}
+
+<#
+$folder = ""
+
+$foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+$foldername.Description = "Select a folder"
+if($foldername.ShowDialog() -eq "OK")
+{
+    $folder += $foldername.SelectedPath
+}
+#>
+
+
+if($fixSeason.Length -eq 0)
+{
+    $fs = $FALSE
+}
+else
+{
+    $fs = $TRUE
+}
+if($fixTitle.Length -eq 0)
+{
+    $ft = $FALSE
+}
+else
+{
+    $ft = $TRUE
+}
+
+$files = Get-ChildItem "$folder"
+
+If($fs)
+{
+    ForEach($file in $files){
+        If( $file -match "\w+.S\d\dE\d+")
+        {   
+            $oldSeason = $file.Name[$file.Name.IndexOf('.')+2]+$file.Name[$file.Name.IndexOf('.')+3]
+            $stitle = $file.Name.Replace(".S$oldSeason",".S$FixSeason")            
+            Rename-Item -Path $file.PSPath -NewName $stitle
+            #"Match - Fix Season"
+        }
+    }
+}
+If($ft)
+{
+    ForEach($file in $files){
+        If( $file -match "\w+.S\d\dE\d+")
+        {
+            $oldTitle = ""
+            ForEach($i in 0..($file.Name.IndexOf('.')-1)){
+                $oldTitle = $oldTitle + $file.Name[$i]
+            }
+            
+            $ttitle = $file.Name.Replace("$oldTitle","$FixTitle")   
+            Rename-Item -Path $file.PSPath -NewName $ttitle
+
+            #"Match - Fix Title"
+            #$oldTitle
+        }
+        else
+        {
+            "No Match - Fix Title"
+        }
+    }
+}
+
+If(-not $fs -and -not $ft)
+{
+    $files = Get-ChildItem "$folder"
+    $title = "$title.S$season" + "E"
+    ForEach($file in $files){
+        If( $file -match "\d+ \w+ -?\d+")
+        {
+            $bfileName = ""
+            $bfileName = $filename.Name
+            $regFile = $file.Name | Select-String -Pattern '(^\d+)'
+
+            $episode = $regFile.Matches.Value
+
+            Rename-Item -Path $file.PSPath -NewName "$title$episode"
+        }
+        else
+        {
+            Write-Host "No Match - Standard"
+        }
+    }
+}
